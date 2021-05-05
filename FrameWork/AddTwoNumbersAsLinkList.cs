193415742f06dcf98ee2e229ecdf99d8d90683f9
Jerry <t123885@gmail.com>
2021-05-05 14:29:23 +0800

    add test case for merge two linklist solution

--- FrameWork/AddTwoNumbersAsLinkList.cs
+++ FrameWork/AddTwoNumbersAsLinkList.cs
@@ -6,29 +6,50 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    public int GetNumber()
+    {
+        ListNode a = this;
+        int layerNumber = 0;
+        int number = 0;
+        while(a != null){
+            number += (a.val) * (int)Math.Pow(10,layerNumber);;
+            a = a.next;
+            layerNumber ++;
+        }
+        return number;
+    }
 }
 
 public class AddTwoNumbersAsLinkList {
     public ListNode AddTwoNumbers(ListNode a, ListNode b) {
-        Console.WriteLine("Add two numbers");
         ListNode la = a;
         ListNode lb = b;
-        ListNode r1 = new ListNode();
+        ListNode r,ri = new ListNode();
+        r = ri;
         int carry = 0;
 
-        while (la != null || lb != null)
+        while (la != null || lb != null || ri != null)
         {
             var b_value = (lb == null ? 0 : lb.val); 
             var a_value = (la == null ? 0 : la.val); 
             int total = b_value + a_value + carry;
-            r1.val = total % 10;
+            ri.val = total % 10;
             carry  = total / 10;
-            r1.next = new ListNode();
-            Console.WriteLine($"la val:{a_value} p2 val:{b_value} r1:{r1.val}");
+            if(la != null && la.next != null){
+                ri.next = new ListNode();
+            }
+            if(lb != null && lb.next != null){
+                ri.next = new ListNode();
+            }
+            if(carry == 1){
+                ri.next = new ListNode();
+            }
+            Console.WriteLine($"la val:{a_value} p2 val:{b_value} r1:{ri.val} carry:{carry}");
             lb = lb == null ? null : lb.next; 
             la = la == null ? null : la.next;
-            r1 = r1 == null ? null : r1.next;
+            ri = ri.next ; 
         }
-        return la;
+        return r;
     }
 }
--- Solution.Test/Solution_TestAll.cs
+++ Solution.Test/Solution_TestAll.cs
@@ -27,13 +27,30 @@ namespace Solution
         }
 
         [Test]
-        public void AddTwoNumbers(){
-            ListNode l1 = new ListNode(1);
-            ListNode l2 = new ListNode(2);
-            addTwoNumbersAsLinkList.AddTwoNumbers(l1,l2);
-            TestContext.Progress.WriteLine("s");
+        public void AddTwoLinkList_342n465_Return807(){
+            ListNode a1 = new ListNode();
+            ListNode a2 = new ListNode();
+            ListNode a3 = new ListNode();
+            ListNode b1 = new ListNode();
+            ListNode b2 = new ListNode();
+            ListNode b3 = new ListNode();
+            a1.val = 2;
+            a2.val = 4;
+            a3.val = 3;
+            b1.val = 5;
+            b2.val = 6;
+            b3.val = 4;
+            a1.next = a2;
+            a2.next = a3;
+            b1.next = b2;
+            b2.next = b3;
+            AddTwoNumbersAsLinkList addTwoNumbersAsLinkList = new AddTwoNumbersAsLinkList();
+            var a = addTwoNumbersAsLinkList.AddTwoNumbers(a1,b1);
+            Assert.AreEqual(807,a.GetNumber());
         }
-        public void AddTwoLinkList_243n56_Return704(){
+
+        [Test]
+        public void AddTwoLinkList_342n65_Return704(){
             ListNode a1 = new ListNode();
             ListNode a2 = new ListNode();
             ListNode a3 = new ListNode();
@@ -48,8 +65,24 @@ namespace Solution
             a2.next = a3;
             b1.next = b2;
             AddTwoNumbersAsLinkList addTwoNumbersAsLinkList = new AddTwoNumbersAsLinkList();
-            addTwoNumbersAsLinkList.AddTwoNumbers(a1,b1);
-            //Assert.AreEqual(new int[]{7,0,4},)
+            var mergedList = addTwoNumbersAsLinkList.AddTwoNumbers(a1,b1);
+            Assert.AreEqual(407,mergedList.GetNumber());
         }
+
+        [Test]
+        public void AddTwoLinkList_45n55_Return100(){
+            ListNode a1 = new ListNode();
+            ListNode a2 = new ListNode();
+            ListNode b1 = new ListNode();
+            ListNode b2 = new ListNode();
+            a1.val = 5;
+            a2.val = 4;
+            b1.val = 5;
+            b2.val = 5;
+            a1.next = a2;
+            b1.next = b2;
+            var mergedList = addTwoNumbersAsLinkList.AddTwoNumbers(a1,b1);
+            Assert.AreEqual(100,mergedList.GetNumber());
+        } 
     }
 }
